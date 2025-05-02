94e7ea4bf7e56b5772224427e6fd015fd6f0aaa3
Franco4433 <francoezequielvedia2002@gmail.com>
2025-05-02 18:40:25 -0300

    Clase-2-Informatorio_30-04-25

---
+++ Clase_2_30-04-25/Estructuras-control.py
@@ -0,0 +1,6 @@
+#iterativo
+
+variable = [1,2,34,55,58]
+
+for i in variable: 
+    print ("estoy en este elemento: ", i)
---
+++ Clase_2_30-04-25/Operaciones.py
@@ -0,0 +1,10 @@
+# suma
+int_1=4;
+int_2=5;
+
+#suma =int_1+int_2
+#print (f"La suma de los numeros {int_1} y {int_2} es: {suma}") 
+
+diccionario= {'5657657': 'apex', '5657655': 'atento' }
+
+print(diccionario)
